fd1c27813a9d13752c51121067787586e6b6a81f
flameleo11 <flameleo11@foxmail.com>
2020-06-18 18:03:37 +0800

    if fix mod feature by me

--- mod.js
+++ mod.js
@@ -6,7 +6,8 @@ function auto_disable_failed_mod() {
 function main() {
 	var arr = []
 	for (var i = 0; i < arr.length; i++) {
-		auto_disable_failed_mod()
+		// turn off this feature
+		// auto_disable_failed_mod()
 	}
 }
 
